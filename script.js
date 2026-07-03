b3353bce760e6c209d7b436e2de683e512ce6642
chiru-1992 <chirugurung730@gmail.com>
2026-07-03 19:51:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const moroccoPlayers = [
+  { name: "Achraf Hakimi", image: "images/morocco/hakimi.png" },
+  { name: "Brahim Diaz", image: "images/morocco/brahim.png" },
+  // ...continue until all 23 Morocco players
+];
+
+const canadaPlayers = [
+  { name: "Jonathan David", image: "images/canada/jonathan_david.png" },
+  { name: "Alphonso Davies", image: "images/canada/alphonso_davies.png" },
+  // ...continue until all 23 Canada players
+];
