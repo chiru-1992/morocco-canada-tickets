7533266b392a1341c81205b55de270655bb584eb
chiru-1992 <chirugurung730@gmail.com>
2026-07-04 07:29:38 +0530

    Update players.js

--- players.js
+++ players.js
@@ -1,254 +1,117 @@
-
 const tickets = [
 {
-  ticket: 1,
-  morocco: {
-    name: "Achraf Hakimi",
-    image: "players/Achraf Hakimi.png"
-  },
-  canada: {
-    name: "Jonathan David",
-    image: "players/Jonathan David.png"
-  }
+ticket:1,
+morocco:{name:"Achraf Hakimi",image:"players/achraf_hakimi.jpg"},
+canada:{name:"Jonathan David",image:"players/jonathan_david.jpg"}
 },
 {
-  ticket: 2,
-  morocco: {
-    name: "Noussair Mazraoui",
-    image: "players/Noussair Mazraoui.png"
-  },
-  canada: {
-    name: "Alphonso Davies",
-    image: "players/Alphonso Davies.png"
-  }
+ticket:2,
+morocco:{name:"Noussair Mazraoui",image:"players/noussair_mazraoui.jpg"},
+canada:{name:"Alphonso Davies",image:"players/alphonso_davies.jpg"}
 },
 {
-  ticket: 3,
-  morocco: {
-    name: "Nayef Aguerd",
-    image: "players/Nayef Aguerd.png"
-  },
-  canada: {
-    name: "Cyle Larin",
-    image: "players/Cyle Larin.png"
-  }
+ticket:3,
+morocco:{name:"Nayef Aguerd",image:"players/nayef_aguerd.jpg"},
+canada:{name:"Cyle Larin",image:"players/cyle_larin.jpg"}
 },
 {
-  ticket: 4,
-  morocco: {
-    name: "Jawad El Yamiq",
-    image: "players/Jawad El Yamiq.png"
-  },
-  canada: {
-    name: "Tajon Buchanan",
-    image: "players/Tajon Buchanan.png"
-  }
+ticket:4,
+morocco:{name:"Jawad El Yamiq",image:"players/jawad_el_yamiq.jpg"},
+canada:{name:"Tajon Buchanan",image:"players/tajon_buchanan.jpg"}
 },
 {
-  ticket: 5,
-  morocco: {
-    name: "Chadi Riad",
-    image: "players/Chadi Riad.png"
-  },
-  canada: {
-    name: "Stephen Eustaquio",
-    image: "players/Stephen Eustaquio.png"
-  }
+ticket:5,
+morocco:{name:"Chadi Riad",image:"players/chadi_riad.jpg"},
+canada:{name:"Stephen Eustaquio",image:"players/stephen_eustaquio.jpg"}
 },
 {
-  ticket: 6,
-  morocco: {
-    name: "Youssef Belammari",
-    image: "players/Youssef Belammari.png"
-  },
-  canada: {
-    name: "Ismael Kone",
-    image: "players/Ismael Kone.png"
-  }
+ticket:6,
+morocco:{name:"Youssef Belammari",image:"players/youssef_belammari.jpg"},
+canada:{name:"Ismael Kone",image:"players/ismael_kone.jpg"}
 },
 {
-  ticket: 7,
-  morocco: {
-    name: "Bilal El Khannouss",
-    image: "players/Bilal El Khannouss.png"
-  },
-  canada: {
-    name: "Mathieu Choiniere",
-    image: "players/Mathieu Choiniere.png"
-  }
+ticket:7,
+morocco:{name:"Bilal El Khannouss",image:"players/bilal_el_khannouss.jpg"},
+canada:{name:"Mathieu Choiniere",image:"players/mathieu_choiniere.jpg"}
 },
 {
-  ticket: 8,
-  morocco: {
-    name: "Sofyan Amrabat",
-    image: "players/Sofyan Amrabat.png"
-  },
-  canada: {
-    name: "Jonathan Osorio",
-    image: "players/Jonathan Osorio.png"
-  }
-},{
-  ticket: 9,
-  morocco: {
-    name: "Azzedine Ounahi",
-    image: "players/Azzedine Ounahi.png"
-  },
-  canada: {
-    name: "Nathan Saliba",
-    image: "players/Nathan Saliba.png"
-  }
+ticket:8,
+morocco:{name:"Sofyan Amrabat",image:"players/sofyan_amrabat.jpg"},
+canada:{name:"Jonathan Osorio",image:"players/jonathan_osorio.jpg"}
 },
 {
-  ticket: 10,
-  morocco: {
-    name: "Ismael Saibari",
-    image: "players/Ismael Saibari.png"
-  },
-  canada: {
-    name: "Ali Ahmed",
-    image: "players/Ali Ahmed.png"
-  }
+ticket:9,
+morocco:{name:"Azzedine Ounahi",image:"players/azzedine_punahi.jpg"},
+canada:{name:"Nathan Saliba",image:"players/nathan_saliba.jpg"}
 },
 {
-  ticket: 11,
-  morocco: {
-    name: "Neil El Aynaoui",
-    image: "players/Neil El Aynaoui.png"
-  },
-  canada: {
-    name: "Niko Sigur",
-    image: "players/Niko Sigur.png"
-  }
+ticket:10,
+morocco:{name:"Ismael Saibari",image:"players/ismael_saibari.jpg"},
+canada:{name:"Ali Ahmed",image:"players/ali_ahmed.jpg"}
 },
 {
-  ticket: 12,
-  morocco: {
-    name: "Brahim Diaz",
-    image: "players/Brahim Diaz.png"
-  },
-  canada: {
-    name: "Richie Laryea",
-    image: "players/Richie Laryea.png"
-  }
+ticket:11,
+morocco:{name:"Neil El Aynaoui",image:"players/neil_el_aynaoui.jpg"},
+canada:{name:"Niko Sigur",image:"players/niko_sigur.jpg"}
 },
 {
-  ticket: 13,
-  morocco: {
-    name: "Soufiane Rahimi",
-    image: "players/Soufiane Rahimi.png"
-  },
-  canada: {
-    name: "Alistair Johnston",
-    image: "players/Alistair Johnston.png"
-  }
+ticket:12,
+morocco:{name:"Brahim Diaz",image:"players/brahim_diaz.jpg"},
+canada:{name:"Richie Laryea",image:"players/richie_laryea.jpg"}
 },
 {
-  ticket: 14,
-  morocco: {
-    name: "Ayoub El Kaabi",
-    image: "players/Ayoub El Kaabi.png"
-  },
-  canada: {
-    name: "Derek Cornelius",
-    image: "players/Derek Cornelius.png"
-  }
+ticket:13,
+morocco:{name:"Soufiane Rahimi",image:"players/soufiane_rahimi.jpg"},
+canada:{name:"Alistair Johnston",image:"players/alistair_johnston.jpg"}
 },
 {
-  ticket: 15,
-  morocco: {
-    name: "Chemsdine Talbi",
-    image: "players/Chemsdine Talbi.png"
-  },
-  canada: {
-    name: "Moise Bombito",
-    image: "players/Moise Bombito.png"
-  }
+ticket:14,
+morocco:{name:"Ayoub El Kaabi",image:"players/ayoub_el_kaabi.jpg"},
+canada:{name:"Derek Cornelius",image:"players/derek_cornelius.jpg"}
 },
 {
-  ticket: 16,
-  morocco: {
-    name: "Amine Sbai",
-    image: "players/Amine Sbai.png"
-  },
-  canada: {
-    name: "Joel Waterman",
-    image: "players/Joel Waterman.png"
-  }
-},{
-  ticket: 17,
-  morocco: {
-    name: "Zakaria El Ouahdi",
-    image: "players/Zakaria El Ouahdi.png"
-  },
-  canada: {
-    name: "Luc de Fougerolles",
-    image: "players/Luc de Fougerolles.png"
-  }
+ticket:15,
+morocco:{name:"Chemsdine Talbi",image:"players/chemsdine_talbi.jpg"},
+canada:{name:"Moise Bombito",image:"players/moise_mombito.jpg"}
 },
 {
-  ticket: 18,
-  morocco: {
-    name: "Marwane Saadane",
-    image: "players/Marwane Saadane.png"
-  },
-  canada: {
-    name: "Jacob Shaffelburg",
-    image: "players/Jacob Shaffelburg.png"
-  }
+ticket:16,
+morocco:{name:"Amine Sbai",image:"players/amine_sbai.jpg"},
+canada:{name:"Joel Waterman",image:"players/joel_waterman.jpg"}
 },
 {
-  ticket: 19,
-  morocco: {
-    name: "Anass Salah-Eddine",
-    image: "players/Anass Salah-Eddine.png"
-  },
-  canada: {
-    name: "Promise David",
-    image: "players/Promise David.png"
-  }
+ticket:17,
+morocco:{name:"Zakaria El Ouahdi",image:"players/zakaria_el_ouhdi.jpg"},
+canada:{name:"Luc de Fougerolles",image:"players/luc_de_fougerolles.jpg"}
 },
 {
-  ticket: 20,
-  morocco: {
-    name: "Redouane Halhal",
-    image: "players/Redouane Halhal.png"
-  },
-  canada: {
-    name: "Tani Oluwaseyi",
-    image: "players/Tani Oluwaseyi.png"
-  }
+ticket:18,
+morocco:{name:"Marwane Saadane",image:"players/marwane_saadane.jpg"},
+canada:{name:"Jacob Shaffelburg",image:"players/jacob_shaffelburg.jpg"}
 },
 {
-  ticket: 21,
-  morocco: {
-    name: "Samir El Mourabet",
-    image: "players/Samir El Mourabet.png"
-  },
-  canada: {
-    name: "Jayden Nelson",
-    image: "players/Jayden Nelson.png"
-  }
+ticket:19,
+morocco:{name:"Anass Salah-Eddine",image:"players/anass_salah_eddini.jpg"},
+canada:{name:"Promise David",image:"players/promise_david.jpg"}
 },
 {
-  ticket: 22,
-  morocco: {
-    name: "Ayyoub Bouaddi",
-    image: "players/Ayyoub Bouaddi.png"
-  },
-  canada: {
-    name: "Liam Millar",
-    image: "players/Liam Millar.png"
-  }
+ticket:20,
+morocco:{name:"Redouane Halhal",image:"players/redouane_halhal.jpg"},
+canada:{name:"Tani Oluwaseyi",image:"players/tani_oluwasyi.jpg"}
 },
 {
-  ticket: 23,
-  morocco: {
-    name: "Gessime Yassine",
-    image: "players/Gessime Yassine.png"
-  },
-  canada: {
-    name: "Theo Bair",
-    image: "players/Theo Bair.png"
-  }
+ticket:21,
+morocco:{name:"Samir El Mourabet",image:"players/samir_el_mourabet.jpg"},
+canada:{name:"Jayden Nelson",image:"players/jayden_nelson.jpg"}
+},
+{
+ticket:22,
+morocco:{name:"Ayyoub Bouaddi",image:"players/ayoub_ouaddi.jpg"},
+canada:{name:"Liam Millar",image:"players/liam_millar.jpg"}
+},
+{
+ticket:23,
+morocco:{name:"Gessime Yassine",image:"players/gessime_yassine.jpg"},
+canada:{name:"Theo Bair",image:"players/theo_bair.jpg"}
 }
 ];
